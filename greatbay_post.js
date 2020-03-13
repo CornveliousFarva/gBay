00bbe32bd32d60b427f01a21c20d91b58de3611f
Amy Fabella <amyfabella@hotmail.com>
2020-03-12 21:25:53 -0700

    removed unnecessary fs code

--- greatbay_post.js
+++ greatbay_post.js
@@ -37,20 +37,8 @@ inquirer.prompt([
     message: "How many of these products are you selling?",
     name: "quantity",
   },
-]).then(function(data) {
 
-  var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-
-  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-
-    if (err) {
-      return console.log(err);
-    }
-
-    console.log("Product input successfully gathered!");
-
-  });
-});
+]);
 
 function postProduct() {
   console.log("Posting a new product...\n");
